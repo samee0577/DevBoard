1e87d9a9d9faa7f09c12d7db28de5469f32cbe16
samee <shaikhsamee0577@gmail.com>
2026-08-07 12:16:15 +0530

    Add CORS config for production and dynamic port

--- devboard-server/index.js
+++ devboard-server/index.js
@@ -7,10 +7,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const port = 3001;
+const port = process.env.PORT||3001;
 
 const { PGHOST,
     PGDATABASE,
