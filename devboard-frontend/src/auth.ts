faa2f13b31285503ac256081cdf05d68902007a7
samee <shaikhsamee0577@gmail.com>
2026-09-13 21:11:26 +0530

    renaming

--- devboard-frontend/src/auth.ts
+++ devboard-frontend/src/auth.ts
@@ -1,9 +1,9 @@
 import { createAuthClient } from '@neondatabase/auth';
 
-const neonAuthUrl = import.meta.env.VITE_NEON_AUTH_URL;
+const neonAuthUrl = import.meta.env.NEON_AUTH_URL;
 console.log(import.meta.env);
 if (!neonAuthUrl) {
-  throw new Error('Missing VITE_NEON_AUTH_URL. Add your Neon Auth URL in the frontend .env file.');
+  throw new Error('Missing NEON_AUTH_URL. Add your Neon Auth URL in the frontend .env file.');
 }
 
 export const authClient = createAuthClient(neonAuthUrl);
