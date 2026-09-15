9deb9e0e440c85539572bb690fac3c06fc297052
samee0577 <shaikhsamee0577@gmail.com>
2026-09-15 14:18:30 +0530

    Mobile responsive

--- devboard-frontend/src/features/projects/component/NavBar.tsx
+++ devboard-frontend/src/features/projects/component/NavBar.tsx
@@ -10,8 +10,10 @@ export default function Navbar() {
     })
 
     return (
-        <nav style={{ margin: "5px" }}>
-            <div><h1>DEVBOARD</h1></div>
+        <nav>
+            <div>
+                <h1>DEVBOARD</h1>
+            </div>
             <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
                 <Link className="buttonStyle" to="/dashboard">Dashboard</Link>
                 <Link className="buttonStyle" to="/newProject">+ New Project</Link>
