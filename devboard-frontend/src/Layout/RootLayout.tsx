f1d3a0b77c272b2dd02703e4a4679b70d7ee6955
samee <shaikhsamee0577@gmail.com>
2026-09-15 14:12:44 +0530

    ui changes

--- devboard-frontend/src/Layout/RootLayout.tsx
+++ devboard-frontend/src/Layout/RootLayout.tsx
@@ -4,10 +4,10 @@ import NavBar from "../features/projects/component/NavBar"
 export default function RootLayout() {
 
     return (
-        <>
+        <div className="root-layout">
             <NavBar />
             <hr style={{ marginTop: 15, margin: 5 }} />
             <Outlet />
-        </>
+        </div>
     )
 }
